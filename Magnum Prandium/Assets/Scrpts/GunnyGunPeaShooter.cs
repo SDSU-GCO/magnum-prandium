6954df5fff2f4518d3319d1db7a61c5f75d63701
Alexander Evans <alexander.evans@jacks.sdstate.edu>
2018-09-22 16:07:37 -0500

    completed simple projectile weapons
    can now fire a ranged weapon on a cool down and the missle will automatically remove itself after a certain amount of time.

---
+++ Magnum Prandium/Assets/Scrpts/GunnyGunPeaShooter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GunnyGunPeaShooter : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject misslePrefab = null;
+    [SerializeField]
+    float missleSpeed = 1;
+    [SerializeField]
+    float coolDownMissle = 0.25f;
+    float coolDownMissleDefault = 0;
+
+    private void OnEnable()
+    {
+        coolDownMissleDefault = coolDownMissle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coolDownMissle = Mathf.Max(0, coolDownMissle - Time.deltaTime);
+        if (UnityEngine.Input.GetButtonDown("missleAttack"))
+        {
+            missleAttack();
+        }
+    }
+
+    public void missleAttack()
+    {
+        if (coolDownMissle <= 0)
+        {
+            coolDownMissle = coolDownMissleDefault;
+            Vector2 attackAngle = transform.up;
+            GameObject missle = Instantiate(misslePrefab, new Vector3(transform.position.x, transform.position.y, -1), transform.rotation);
+            missle.GetComponent<Rigidbody2D>().velocity = new Vector2(attackAngle.x * missleSpeed, attackAngle.y * missleSpeed);
+        }
+    }
+}
---
+++ Magnum Prandium/Assets/Scrpts/MisslyBangBang.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MisslyBangBang : MonoBehaviour {
+
+    [SerializeField]
+    float lifetimeInSeconds=10;
+
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        lifetimeInSeconds = Mathf.Max(0, lifetimeInSeconds - Time.deltaTime);
+        if(lifetimeInSeconds<=0)
+        {
+            GameObject.Destroy(gameObject);
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer==LayerMask.NameToLayer("Enemy"))
+        {
+            GameObject.Destroy(collision.gameObject);
+        }
+    }
+}
