ef0c810a7857514777ee86a6313a150028d33f84
Swiftfire7 <34386895+Swiftfire7@users.noreply.github.com>
2018-09-22 15:02:49 -0500

    Movement system 2D
    I made a movement script and assigned it to a basic player character

---
+++ Magnum Prandium/Assets/Primitives/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed=1;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+
+        rb.velocity = (movement * speed);
+    }
+}
