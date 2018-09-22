f0a884045279741e4729c2ca668b40703f3bb86d
Alexander Evans <alexander.evans@jacks.sdstate.edu>
2018-09-22 18:45:29 -0500

    removed .meta filter

---
+++ Magnum Prandium/Assets/Scrpts/gameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager gameManager = null;
+
+    private void Awake()
+    {
+        Debug.Assert(gameManager == null, "There should only be one game manager in the game!, found: " + gameManager + " Expected: null");
+        gameManager = this;
+    }
+    private void OnDestroy()
+    {
+        Debug.Assert(gameManager == this, "There should only be one game manager in the game!, found: " + gameManager + " Expected: "+this );
+        gameManager = null;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
