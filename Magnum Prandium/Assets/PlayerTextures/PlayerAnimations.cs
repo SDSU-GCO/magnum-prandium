7aaca3f28ae866c96008aeebc5444fb2beff521e
Genzo Namikawa <30657191+gnamikawa@users.noreply.github.com>
2018-09-23 01:23:43 -0500

    Animation Controllers
    Merging with Win/Loss Conditions.

---
+++ Magnum Prandium/Assets/PlayerTextures/PlayerAnimations.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour {
+
+    Animator animator;
+    public float T = 0.01F;
+
+	// Use this for initialization
+	void Start(){
+		animator = GetComponent<Animator>();
+	    }
+	
+	// Update is called once per frame
+	void Update(){
+		float H = Input.GetAxis("Horizontal");
+		float V = Input.GetAxis("Vertical");
+        //bool  S = Input.GetButtonDown("Shoot");
+        bool  I = (H < T && H > -T);
+
+        animator.SetFloat("HA", H);
+        animator.SetFloat("VA", V);
+        animator.SetBool("Idle", I);
+	    }
+    }
