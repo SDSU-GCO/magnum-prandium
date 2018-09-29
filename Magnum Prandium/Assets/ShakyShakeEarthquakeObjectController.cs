beb6329099cce0e1e96e2d27769e29509963e3df
Alexander Evans <alexander.evans@jacks.sdstate.edu>
2018-09-29 01:27:28 -0500

    implemented compounding Shake2D couroutine
    ...I couldn't sleep so I did it...  The couroutine all seem to excecute in bulk between Update() and LateUpdate() in the excecution order, so I took advantage of that to implement the inverse transformations in the Update() function... Once I figured out the excecution order that wasn't so bad, didn't realize that all the coroutines excecuted in the same place in the loop, that made this SUPER easy upon that realization.

--- Magnum Prandium/Assets/ShakyShakeEarthquakeObjectController.cs
+++ Magnum Prandium/Assets/ShakyShakeEarthquakeObjectController.cs
@@ -14,32 +14,32 @@ public class ShakyShakeEarthquakeObjectController : MonoBehaviour {
     public float fadeIn=0.25f;
     [SerializeField]
     public float fadeOut=0.25f;
-
-    Coroutine thisObjectsLastShake = null;
-    bool shaking = false;
     
-    Vector3 originalPosition;
-    Quaternion originalRotation;
+    private struct Transformation
+    {
+        public Vector2 vector2;
+        public float rotation;
+    }
+
+    private List<Transformation> transformations = new List<Transformation>();
 
     public void rockTheBoat()
     {
-        if (shaking == true && thisObjectsLastShake !=null)
+        StartCoroutine(ShakeObject2D(duration, magnitudePos, magnitudeRot, fadeIn, fadeOut));
+    }
+
+    public void Update()
+    {
+        foreach(Transformation transformation in transformations)
         {
-            StopCoroutine(thisObjectsLastShake);
-            transform.localPosition = originalPosition;
-            transform.localRotation = originalRotation;
-            shaking = false;
+            transform.position = new Vector2(transform.position.x - transformation.vector2.x, transform.position.y - transformation.vector2.y);
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.y, transform.rotation.z - transformation.rotation);
         }
-        originalPosition = transform.localPosition;
-        originalRotation = transform.localRotation;
-        thisObjectsLastShake = StartCoroutine(ShakeObject2D(duration, magnitudePos, magnitudeRot, fadeIn, fadeOut));
+        transformations.Clear();
     }
 
     IEnumerator ShakeObject2D(float duration, float magnitudePos, float magnitudeRot, float fadeIn, float fadeOut)
     {
-        shaking = true;
-        Vector3 originalPosition = transform.localPosition;
-        Quaternion originalRotation = transform.localRotation;
         float currentDuration = 0;
         float fadeInCompletedTime = duration * fadeIn;
         float fadeOutStartTime = duration - (duration * fadeOut);
@@ -61,22 +61,24 @@ public class ShakyShakeEarthquakeObjectController : MonoBehaviour {
             }
             else if(currentDuration>=fadeOutStartTime && fadeOutStartTime != duration)
             {
-                currentMagnitudePos = magnitudePos * (1 - ((currentDuration - fadeOutStartTime) / (duration - fadeOutStartTime)));
-                currentMagnitudeRot = magnitudeRot * (1 - ((currentDuration - fadeOutStartTime) / (duration - fadeOutStartTime)));
+                currentMagnitudePos = magnitudePos * (1 + ((fadeOutStartTime-currentDuration) / (duration - fadeOutStartTime)));
+                currentMagnitudeRot = magnitudeRot * (1 + ((fadeOutStartTime-currentDuration) / (duration - fadeOutStartTime)));
             }
             else
             {
                 currentMagnitudePos = magnitudePos;
                 currentMagnitudeRot = magnitudeRot;
             }
-            transform.localPosition = new Vector2(originalPosition.x + (Random.Range(-currentMagnitudePos, currentMagnitudePos)), originalPosition.y + (Random.Range(-currentMagnitudePos, currentMagnitudePos)));
-            transform.localRotation= Quaternion.Euler(originalRotation.eulerAngles.x, originalRotation.y, originalRotation.z + (Random.Range(-currentMagnitudeRot, currentMagnitudeRot)));
+            
+            Vector2 tempVector = new Vector2((Random.Range(-currentMagnitudePos, currentMagnitudePos)), (Random.Range(-currentMagnitudePos, currentMagnitudePos)));
+            Transformation transformation = new Transformation();
+            transformation.vector2 = tempVector;
+            transformation.rotation = (Random.Range(-currentMagnitudeRot, currentMagnitudeRot));
+            transform.position = new Vector2(transform.position.x + tempVector.x, transform.position.y + tempVector.y);
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.y, transform.rotation.z + transformation.rotation);
+            transformations.Add(transformation);
             
             yield return null;
         }
-
-        shaking = false;
-        transform.localPosition = originalPosition;
-        transform.localRotation = originalRotation;
     }
 }
