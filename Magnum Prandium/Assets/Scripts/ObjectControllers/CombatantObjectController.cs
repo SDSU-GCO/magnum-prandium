e4d15cc2836757be7aa248343659eaa46f2c7630
Alexander Evans <alexander.evans@jacks.sdstate.edu>
2018-10-03 01:52:22 -0500

    Added combatant stats
    I added stats for combatants, and began designing a system for applying temporary moddifiers to stats and resistances.

--- Magnum Prandium/Assets/Scripts/ObjectControllers/CombatantObjectController.cs
+++ Magnum Prandium/Assets/Scripts/ObjectControllers/CombatantObjectController.cs
@@ -5,45 +5,148 @@ using System;
 
 public class CombatantObjectController : MonoBehaviour
 {
-    public enum Attribute { FIRE, LAVA, ICE, AIR, WATER, VOID, DEATH, LIFE, LIGHTNING }
+    public enum Attribute { FIRE, LAVA, ICE, AIR, WATER, VOID, DEATH, LIFE, LIGHTNING, LIGHT, DARK }
     
-    [Serializable]
-    public struct AttributeValue
+    public struct Modifiers
     {
-        public Attribute attribute;
-        public float multiplier;
-        public AttributeValue(Attribute _attribute, float _multiplier)
+        public float baseResistance;
+        public Dictionary<string, float> tempModifiers;
+
+        public Modifiers(float baseModifier, Dictionary<string, float> tempModifiers)
         {
-            attribute = _attribute;
-            multiplier = _multiplier;
+            this.baseResistance = baseModifier;
+            this.tempModifiers = tempModifiers;
+        }
+
+        public Modifiers(float baseModifier)
+        {
+            this.baseResistance = baseModifier;
+            this.tempModifiers = new Dictionary<string, float>();
         }
     }
 
-    
-    [Serializable]
-    public class Stats
-    {
-        public float hp;
-        public float mp;
-        public List<AttributeValue> resistances = new List<AttributeValue>()
-        {
-            {new AttributeValue( Attribute.FIRE, 1.0f ) },
-            {new AttributeValue( Attribute.LAVA, 1.0f ) },
-            {new AttributeValue( Attribute.ICE, 1.0f ) },
-            {new AttributeValue( Attribute.WATER, 1.0f ) },
-            {new AttributeValue( Attribute.VOID, 1.0f ) },
-            {new AttributeValue( Attribute.DEATH, 1.0f ) },
-            {new AttributeValue( Attribute.LIFE, 1.0f ) },
-            {new AttributeValue( Attribute.LIGHTNING, 1.0f ) },
-            {new AttributeValue( Attribute.AIR, 1.0f ) },
-        };
 
+    [Serializable]
+    public struct Stat
+    {
+        public float baseStat;
+        public Dictionary<string, float> tempStatModifiers;
+
+        public Stat(float baseStat, Dictionary<string, float> tempStatModifiers)
+        {
+            this.baseStat = baseStat;
+            this.tempStatModifiers = tempStatModifiers;
+        }
+
+        public Stat(float baseStat)
+        {
+            this.baseStat = baseStat;
+            tempStatModifiers = new Dictionary<string, float>();
+        }
+    }
+
+    [Serializable]
+    class Resistances
+    {
+        public float fire = 1;
+        public float lava = 1;
+        public float ice = 1;
+        public float air = 1;
+        public float water = 1;
+        public float voidElem = 1;
+        public float death = 1;
+        public float life = 1;
+        public float lightning = 1;
+        public float light = 1;
+        public float dark = 1;
+    }
+
+    [Serializable]
+    public class BaseStatBlock
+    {
+        public float hp = 1;
+        public float mp = 1;
+        public float strength = 1;
+        public float defense = 1;
+        public float stamina = 1;
+        public float dexterity = 1;
+        public float intelligence = 1;
+        public float critRate = 1;
+        public float luck = 1;
+    }
+
+    [Serializable]
+    public struct StatBlock
+    {
+        public Stat hp;
+        public Stat mp;
+        public Stat strength;
+        public Stat defense;
+        public Stat stamina;
+        public Stat dexterity;
+        public Stat intelligence;
+        public Stat critRate;
+        public Stat luck;
+    }
+
+    [Serializable]
+    struct CombatantStats
+    {
+        public BaseStatBlock baseStats;
+        public Resistances baseResistances;
     }
     
+    [Serializable]
+    public struct Stats
+    {
+        public StatBlock statBlock;
+        public Dictionary<Attribute, Modifiers> resistances;
+    }
+
+    [SerializeField]
+    CombatantStats combatantStats;
+
+    [NonSerialized]
     public Stats stats;
 
-	// Use this for initialization
-	void Start () {
+    private void Awake()
+    {
+        CopyStatsToRuntimeStructure(combatantStats.baseStats, stats.statBlock);
+        ResistancesToDictionary(ref combatantStats.baseResistances, stats.resistances);
+    }
+
+    private void CopyStatsToRuntimeStructure(BaseStatBlock stats, StatBlock statBlock)
+    {
+        statBlock.hp.baseStat = stats.hp;
+        statBlock.mp.baseStat = stats.mp;
+        statBlock.strength.baseStat = stats.strength;
+        statBlock.defense.baseStat = stats.defense;
+        statBlock.stamina.baseStat = stats.stamina;
+        statBlock.dexterity.baseStat = stats.dexterity;
+        statBlock.intelligence.baseStat = stats.intelligence;
+        statBlock.critRate.baseStat = stats.critRate;
+        statBlock.luck.baseStat = stats.luck;
+    }
+
+    void ResistancesToDictionary(ref Resistances resistances, Dictionary<Attribute, Modifiers> resistancesList)
+    {
+        resistancesList = new Dictionary<Attribute, Modifiers>();
+        resistancesList.Add(Attribute.AIR, new Modifiers(resistances.air));
+        resistancesList.Add(Attribute.FIRE, new Modifiers(resistances.fire));
+        resistancesList.Add(Attribute.ICE, new Modifiers(resistances.ice));
+        resistancesList.Add(Attribute.LAVA, new Modifiers(resistances.lava));
+        resistancesList.Add(Attribute.WATER, new Modifiers(resistances.water));
+        resistancesList.Add(Attribute.VOID, new Modifiers(resistances.voidElem));
+        resistancesList.Add(Attribute.DEATH, new Modifiers(resistances.death));
+        resistancesList.Add(Attribute.LIFE, new Modifiers(resistances.life));
+        resistancesList.Add(Attribute.LIGHTNING, new Modifiers(resistances.lightning));
+        resistancesList.Add(Attribute.LIGHT, new Modifiers(resistances.light));
+        resistancesList.Add(Attribute.DARK, new Modifiers(resistances.dark));
+    }
+
+    // Use this for initialization
+    void Start () {
+
 	}
 	
 	// Update is called once per frame
