4e38ef06920b7615fc72de116d90671dca880fcd
Alexander Evans <alexander.evans@jacks.sdstate.edu>
2018-10-07 00:03:18 -0500

    Started tile system interface

---
+++ Magnum Prandium/Assets/Scripts/SceneData/TileMapSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+public class TileMapSystem : MonoBehaviour {
+    
+    
+    Tilemap tilemap;
+
+    // Use this for initialization
+    void Start () {
+        Tile.ColliderType colliderType = tilemap.GetColliderType(new Vector3Int(0,0,0));
+        tilemap.GetBoundsLocal(new Vector3Int(0, 0, 0));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
